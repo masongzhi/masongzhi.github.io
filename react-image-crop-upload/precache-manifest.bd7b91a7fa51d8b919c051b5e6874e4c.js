11541f2ed7278c150ba4cd54ceebe0bc6dd3b419
masongzhi <511071161@qq.com>
2019-02-28 10:42:57 +0800

    增加react-image-crop-upload

---
+++ react-image-crop-upload/precache-manifest.bd7b91a7fa51d8b919c051b5e6874e4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "95685a757304b5f35824",
+    "url": "/static/js/main.95685a75.chunk.js"
+  },
+  {
+    "revision": "5468659aa3493bcb6f6a",
+    "url": "/static/js/2.5468659a.chunk.js"
+  },
+  {
+    "revision": "95685a757304b5f35824",
+    "url": "/static/css/main.76c144f6.chunk.css"
+  },
+  {
+    "revision": "5468659aa3493bcb6f6a",
+    "url": "/static/css/2.85b360ea.chunk.css"
+  },
+  {
+    "revision": "6319aa915ec3c76be7257a3bef3481f6",
+    "url": "/index.html"
+  }
+];
