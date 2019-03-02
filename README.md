a829ab604a79ff65ec43e7ce754c10cba4995204
masongzhi <511071161@qq.com>
2019-03-02 15:23:44 +0800

    添加七牛云上传头像工具react-qiniu-avatar-upload

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ react-image-crop-upload
 =====
 http://masongzhi.github.io/react-image-crop-upload
 ![WX20190228-103838@2x.png](https://upload-images.jianshu.io/upload_images/3708358-dd37c155ab2ac2d5.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+react-qiniu-avatar-upload
+=====
+http://masongzhi.github.io/react-qiniu-avatar-upload
+![WX20190228-103838@2x.png](https://upload-images.jianshu.io/upload_images/3708358-dd37c155ab2ac2d5.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
---
+++ react-qiniu-avatar-upload/precache-manifest.9d41a62b09a4f4a09ea54c2e21ca32dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7d7b403e27f2134e1701",
+    "url": "/static/js/main.7d7b403e.chunk.js"
+  },
+  {
+    "revision": "40e46860d101a02b0802",
+    "url": "/static/js/2.40e46860.chunk.js"
+  },
+  {
+    "revision": "7d7b403e27f2134e1701",
+    "url": "/static/css/main.56f25863.chunk.css"
+  },
+  {
+    "revision": "40e46860d101a02b0802",
+    "url": "/static/css/2.83813d12.chunk.css"
+  },
+  {
+    "revision": "dc8e959ad10b7b886f8c5890325b4a7b",
+    "url": "/index.html"
+  }
+];
